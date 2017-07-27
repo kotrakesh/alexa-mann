d27d3134176c863efeb6aa01bb3213e07ee5ea41
YideSong <songide@gmail.com>
2017-07-27 21:42:15 +0800

    combine two server together (alexa skill and microsoft api)

--- python3-sample-rest/connectsample.py
+++ python3-sample-rest/connectsample.py
@@ -12,6 +12,11 @@ import uuid
 import requests
 from flask import Flask, redirect, url_for, session, request, render_template
 from flask_oauthlib.client import OAuth
+from flask_ask import Ask, statement, question, session as ask_session
+
+
+
+
 
 # read private credentials from text file
 client_id, client_secret, *_ = open('_PRIVATE.txt').read().split('\n')
@@ -25,6 +30,9 @@ app = Flask(__name__)
 app.debug = False
 app.secret_key = 'development'
 oauth = OAuth(app)
+ask = Ask(app, "/")
+
+vars = {'date': None, 'time': None, 'duration': None}
 
 # since this sample runs locally without HTTPS, disable InsecureRequestWarning
 requests.packages.urllib3.disable_warnings()
@@ -455,3 +463,102 @@ def convert_amazon_to_ms(Date,Time):
     return date_time
 
 
+@ask.launch
+def welcome():
+    ask_session.attributes['date'] = None #'2017-07-16'
+    ask_session.attributes['time'] = None #'03:00'
+    ask_session.attributes['duration'] = None #'PT5M'
+
+    return question('Hello, please tell me the dates and times when your meeting shall be scheduled')
+
+
+@ask.intent("DateIntent")
+def missing_duration_time(Date):
+    ask_session.attributes['date'] = Date
+
+    if ask_session.attributes['duration'] is None and ask_session.attributes['time'] is None:
+        return question('What time is the meeting and how long is it?')
+    elif ask_session.attributes['duration'] is not None and ask_session.attributes['time'] is None:
+        missing_time(Date, ask_session.attributes['duration'])
+    elif ask_session.attributes['time'] is not None and ask_session.attributes['duration'] is None:
+        missing_duration(ask_session.attributes['date'], ask_session.attributes['time'])
+    else:
+        allKnown(ask_session.attributes['date'], ask_session.attributes['time'], ask_session.attributes['duration'])
+
+
+@ask.intent("TimeIntent")
+def missing_date_duration(Time):
+    ask_session.attributes['time'] = Time
+    if ask_session.attributes['date'] is None and ask_session.attributes['duration'] is None:
+        return question('Whats the date and the duration of the meeting?')
+    elif ask_session.attributes['date'] is not None and ask_session.attributes['duration'] is None:
+        missing_duration(ask_session.attributes['date'], Time)
+    elif ask_session.attributes['date'] is None and ask_session.attributes['duration'] is not None:
+        missing_date(ask_session.attributes['time'], ask_session.attributes['duration'])
+    else:
+        allKnown(ask_session.attributes['date'], ask_session.attributes['time'], ask_session.attributes['duration'])
+
+
+@ask.intent("DurationIntent")
+def missing_date_time(Duration):
+    ask_session.attributes['duration'] = Duration
+    if ask_session.attributes['date'] is None and ask_session.attributes['time'] is None:
+        return question('What day and what time is the meeting?')
+    elif ask_session.attributes['date'] is not None and ask_session.attributes['time'] is None:
+        missing_time(ask_session.attributes['date'], ask_session.attributes['duration'])
+    elif ask_session.attributes['date'] is None and ask_session.attributes['time'] is not None:
+        missing_date(ask_session.attributes['time'], ask_session.attributes['duration'])
+    else:
+        allKnown(ask_session.attributes['date'], ask_session.attributes['time'], ask_session.attributes['duration'])
+
+
+@ask.intent("DateDurationIntent")
+def missing_time(Date, Duration):
+    ask_session.attributes['date'] = Date
+    ask_session.attributes['duration'] = Duration
+    if ask_session.attributes['time'] is None:
+        return question('What time is the meeting?')
+    else:
+        allKnown(ask_session.attributes['date'], ask_session.attributes['time'], ask_session.attributes['duration'])
+
+
+
+@ask.intent("DateTimeIntent")
+def missing_duration(Date, Time):
+    ask_session.attributes['date'] = Date
+    ask_session.attributes['time'] = Time
+    if ask_session.attributes['duration'] is None:
+        return question('How long is the meeting?')
+    else:
+        allKnown(ask_session.attributes['date'], ask_session.attributes['time'], ask_session.attributes['duration'])
+
+
+
+@ask.intent("TimeDurationIntent")
+def missing_date(Time, Duration):
+    ask_session.attributes['duration'] = Duration
+    ask_session.attributes['time'] = Time
+    if ask_session.attributes['date'] is None:
+        return question('What day is the meeting?')
+    else:
+        allKnown(ask_session.attributes['date'], ask_session.attributes['time'], ask_session.attributes['duration'])
+
+
+
+@ask.intent("DataTimeDurationIntent")
+def allKnown(Date, Time, Duration):
+    ask_session.attributes['date'] = Date
+    ask_session.attributes['time'] = Time
+    ask_session.attributes['duration'] = Duration
+    get_infor_from_alexa(Date, Time, Duration)
+    return statement('The meeting is on ' + str(ask_session.attributes['date']) + ' at ' + str(ask_session.attributes['time']) + ' and lasts ' + str(ask_session.attributes['duration']))
+
+def get_infor_from_alexa(Date, Time, Duration):
+    print(Date, Time, Duration)
+    vars['date']=Date
+    vars['time']=Time
+    vars['duration']=Duration
+
+@app.route('/get_information')
+def get_information():
+    return render_template('information.html', aDate=vars['date'], aTime=vars['time'], aDuration =  vars['duration'])
