c14cd57176116e3fa9625cf30afb9bafb62def22
arthurUniMa <awonenbe@mail.uni-mannheim.de>
2017-08-13 22:21:29 +0200

    Created a funktion which returns the endtime of a meeting.

---
+++ python3-sample-rest/durationparser.py
@@ -0,0 +1,26 @@
+import re
+import datetime
+
+def getmeetingendtime(start, duration):
+ if bool(re.search(r'\d*H', duration)):
+    hours = re.search(r'(\d*)H', duration).group(1)
+ else:
+    hours = '00'
+
+ if bool(re.search(r'\d*M', duration)):
+    minutes = re.search(r'(\d*)M', duration).group(1)
+ else:
+    minutes = '00'
+
+ time = hours + ':' + minutes
+
+ timeList = [start, time]
+ sum = datetime.timedelta()
+ for i in timeList:
+    (h, m) = i.split(':')
+    d = datetime.timedelta(hours=int(h), minutes=int(m))
+    sum += d
+
+ str(sum)
+
+#getmeetingendtime("10:20","PT12H5M50S")
