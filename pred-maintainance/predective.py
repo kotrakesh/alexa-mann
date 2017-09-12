dab476411f6a4d83b139a5c4d1908cc1b83120a1
megharangaswamy <meghagowda5193@gmail.com>
2017-09-12 13:28:03 +0200

    made changes

--- pred-maintainance/predective.py
+++ pred-maintainance/predective.py
@@ -1,4 +1,4 @@
-import logging
+ 	import logging
 
 from random import randint
 
@@ -11,75 +11,76 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+
 @ask.launch
-#initial
+
 def predective_maintenance():
 	initial_help = render_template('initialhelp')
 	return question(initial_help)
-#past
+
 @ask.intent("YesterdayIntent")
 def past_yesterday():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
+	currentRecieved = pastYesterday()
 	pastCurrent = render_template('yesterdayCurrent', numbers=currentRecieved)
 	return statement(pastCurrent)
 
 @ask.intent("LastweekIntent")
 def past_week():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
+	currentRecieved = pastlastWeek()
 	pastWeekCurrent = render_template('lastweekCurrent', numbers=currentRecieved)
 	return statement(pastWeekCurrent)
-#present
+
 @ask.intent("TillnowcurrentIntent")
 def present_tillNow():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
+	currentRecieved = tillNowCurrent()
 	presentTillNowCurrent = render_template('todayCurrentTillNow', numbers=currentRecieved)
 	return statement(presentTillNowCurrent)
 
-@ask.intent("NowcurrentIntent")
-def present_now():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
-	presentNowCurrent = render_template('todayCurrentNow', numbers=currentRecieved)
-	return statement(presentTillNowCurrent)
+#@ask.intent("NowcurrentIntent")
+#def present_now():
+#	#urrentRecieved = #-----------------------------------------------------------------
+#	presentNowCurrent = render_template('todayCurrentNow', numbers=currentRecieved)
+#	return statement(presentTillNowCurrent)
 
 @ask.intent("TodayaftercurrentIntent")
 def present_after():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
+	currentRecieved = todayAfterCurrent()
 	presentAfterCurrent = render_template('todayCurrentAfter', numbers=currentRecieved)
 	return statement(presentAfterCurrent) 
 
 @ask.intent("TodayweatherIntent")
 def present_weather():
-	weatherRecieved = [randint(0, 9) for _ in range(1)]
+	weatherRecieved = todayWeather()
 	presentWeather = render_template('todayWeather', numbers=weatherRecieved)
 	return statement(presentWeather) 
 
-@ask.intent("NowweatherIntent")
-def present_Now_weather():
-	weatherRecieved = [randint(0, 9) for _ in range(1)]
-	presentNowWeather = render_template('todayWeatherNow', numbers=weatherRecieved)
-	return statement(presentNowWeather) 
-#future
+#@ask.intent("NowweatherIntent")
+#def present_Now_weather():
+#	weatherRecieved = #------------------------------------------------
+#	presentNowWeather = render_template('todayWeatherNow', numbers=weatherRecieved)
+#	return statement(presentNowWeather) 
+
 @ask.intent("TomorrowcurrentIntent")
 def future_tomorrow_current():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
+	currentRecieved = tomorrowCurrent()
 	futureTomorrowCurrent = render_template('tomorrowCurrent', numbers=currentRecieved)
 	return statement(futureTomorrowCurrent) 
 
 @ask.intent("NextweekcurrentIntent")
 def future_week_current():
-	currentRecieved = [randint(0, 9) for _ in range(1)]
+	currentRecieved = next5DaysCurrent()
 	futureWeekCurrent = render_template('nextweekCurrent', numbers=currentRecieved)
 	return statement(futureWeekCurrent)
 
 @ask.intent("TomorroweatherIntent")
 def future_Tomorrow_weather():
-	weatherRecieved = [randint(0, 9) for _ in range(1)]
+	weatherRecieved = tomorrowWeather()
 	futureTomorrowWeather = render_template('tomorrowWeather', numbers=weatherRecieved)
 	return statement(futureTomorrowWeather)  
 
 @ask.intent("NextweekweatherIntent")
 def future_Week_weather():
-	weatherRecieved = [randint(0, 9) for _ in range(1)]
+	weatherRecieved = Nnext5DaysWeather()
 	futureWeekWeather = render_template('nextweekWeather', numbers=weatherRecieved)
 	return statement(futureWeekWeather)  
 
