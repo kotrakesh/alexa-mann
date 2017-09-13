1e749a07d4e291bcb490421cada20301621c8d0a
Rakesh Kotian <Rakesh Kotian>
2017-09-13 16:54:19 +0200

    python error fix

--- pred-maintainance/app.py
+++ pred-maintainance/app.py
@@ -2,8 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-
-return 'alexa solar pm welcome!'
-
+	return 'alexa solar pm working working!'
 if __name__ == "__main__":
 	app.run()
