f55e590f2dcab50976e4257120369e3d5d335a16
02468 <micha.wiegel@gmail.com>
2017-09-16 20:10:50 +0200

    changed servername

--- roommonitor/run.py
+++ roommonitor/run.py
@@ -3,8 +3,8 @@ To run the app, execute the command "python manage.py runserver" and then
 open a browser and go to http://localhost:5000/
 """
 import flask_script
-import app
+import application
 
-MANAGER = flask_script.Manager(app.app)
-MANAGER.add_command('runserver', flask_script.Server(host='localhost'))
+MANAGER = flask_script.Manager(application.app)
+MANAGER.add_command('runserver', flask_script.Server(host='roommonitor.herokuapp.com'))
 MANAGER.run()
