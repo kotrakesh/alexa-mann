6af8ba17bf6e3d36849b45549cd5946155d26f7b
kotrakesh <kotrakesh@gmail.com>
2017-09-24 11:17:59 +0200

    Changes for conversion factor for power generated

--- pred-maintainance/config.py
+++ pred-maintainance/config.py
@@ -1,4 +1,7 @@
+#db credentials
 dbUser = 'b3465148a734be'
 dbPassowrd = 'a2c4eda1'
 dbHost ='eu-cdbr-west-01.cleardb.com'
 dbDatabase='heroku_c0277ef6294fdf7'
+#electricity power coversion factor
+powerFactor=1.1838
