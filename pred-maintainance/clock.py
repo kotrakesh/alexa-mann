9fdc777a2fb81bba24117f3a281827c601d3fdff
kotrakesh <kotrakesh@gmail.com>
2017-10-08 16:11:44 +0200

    error fix

--- pred-maintainance/clock.py
+++ pred-maintainance/clock.py
@@ -5,6 +5,6 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
 	curcomplete.curcomplete()
-    print('This job is run every 15 minutes.')
+	print('This job is run every 15 minutes.')
 
 sched.start()
