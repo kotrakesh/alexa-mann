c01749f5987bc2e1a0959f8bb5da731510a5e000
kotrakesh <kotrakesh@gmail.com>
2017-10-08 16:53:36 +0200

    future cron

--- pred-maintainance/clock.py
+++ pred-maintainance/clock.py
@@ -1,10 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import curcomplete
+import fordatahd
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=15)
 def timed_job():
 	curcomplete.curcomplete()
 	print('This job is run every 15 minutes.')
+@sched.scheduled_job('cron', hour=17)
+def scheduled_job():
+	fordatahd.fordatahd()
+    print('This job is run everyday at 5pm.')
 
+sched.start()
 sched.start()
