a70b7562cfe72513a6b7c5d9d553bc81e4fd3645
Niklas Glaab <niklas.glaab@web.de>
2017-10-10 12:17:09 +0200

    fixes to sql

--- pred-maintainance/dataBaseAccess_main.py
+++ pred-maintainance/dataBaseAccess_main.py
@@ -7,7 +7,7 @@ class dbc:
 	def pastYesterday(self):
 		cnx = classCommonFunc.dataBaseConnection()
 		cursor = cnx.cursor()
-		query = "SELECT sum(current_output*900) as result  from current_data where city = 'Heidelberg' and curr_timestamp >= curdate()-1 and curr_timestamp <curdate() " #actual SQL statement to be executed
+		query = "SELECT sum(current_output*900/1000000) as result  from current_data where city = 'Heidelberg' and curr_timestamp >= curdate()-1 and curr_timestamp <curdate() " #actual SQL statement to be executed
 		lines = cursor.execute(query) #execute the query
 		data = cursor.fetchall()
 		temp=data
@@ -17,7 +17,7 @@ class dbc:
 	def pastlastWeek(self):
 		cnx = classCommonFunc.dataBaseConnection()
 		cursor = cnx.cursor()
-		query = "SELECT sum(current_output*900) as result  from current_data where city = 'Heidelberg' and curr_timestamp >= curdate()-7 and curr_timestamp <curdate() " #actual SQL statement to be executed
+		query = "SELECT sum(current_output*900/1000000) as result  from current_data where city = 'Heidelberg' and curr_timestamp >= curdate()-7 and curr_timestamp <curdate() " #actual SQL statement to be executed
 		lines = cursor.execute(query) #execute the query
 		data = cursor.fetchall()
 		temp=data
@@ -27,7 +27,7 @@ class dbc:
 	def tillNowCurrent(self):
 		cnx = classCommonFunc.dataBaseConnection()
 		cursor = cnx.cursor()
-		query = "SELECT sum(current_output*900) as result from current_data where curr_timestamp < now() and curr_timestamp >= curdate() and city = 'Heidelberg' " #actual SQL statement to be executed
+		query = "SELECT sum(current_output*900/1000000) as result from current_data where curr_timestamp < now() and curr_timestamp >= curdate() and city = 'Heidelberg' " #actual SQL statement to be executed
 		lines = cursor.execute(query) #execute the query
 		data = cursor.fetchall()
 		temp=data
@@ -37,7 +37,7 @@ class dbc:
 	def todayAfterCurrent(self):
 		cnx = classCommonFunc.dataBaseConnection()
 		cursor = cnx.cursor()
-		query = "SELECT sum(t1.current_output*10800) as result from (select * from future_data where city = 'Heidelberg' order by curr_timestamp desc, weather_date asc limit 8) t1" #actual SQL statement to be executed
+		query = "SELECT sum(t1.current_output*10800/1000000) as result from (select * from future_data where city = 'Heidelberg' order by curr_timestamp desc, weather_date asc limit 8) t1" #actual SQL statement to be executed
 		lines = cursor.execute(query) #execute the query
 		data = cursor.fetchall()
 		temp=data
@@ -57,7 +57,7 @@ class dbc:
 	def tomorrowCurrent(self):
 		cnx = classCommonFunc.dataBaseConnection()
 		cursor = cnx.cursor()
-		query = "SELECT sum(t1.current_output*10800) as result from (select * from future_data where city = 'Heidelberg' and weather_date >= (curdate()+ 1) order by curr_timestamp desc, weather_date asc limit 8) t1" #actual SQL statement to be executed
+		query = "SELECT sum(t1.current_output*10800/1000000) as result from (select * from future_data where city = 'Heidelberg' and weather_date >= (curdate()+ 1) order by curr_timestamp desc, weather_date asc limit 8) t1" #actual SQL statement to be executed
 		lines = cursor.execute(query) #execute the query
 		data = cursor.fetchall()
 		temp=data
@@ -67,7 +67,7 @@ class dbc:
 	def next5DaysCurrent(self):
 		cnx = classCommonFunc.dataBaseConnection()
 		cursor = cnx.cursor()
-		query = "SELECT sum(t1.current_output*10800) as result from (select * from future_data where city = 'Heidelberg' order by curr_timestamp desc, weather_date asc limit 40) t1" #actual SQL statement to be executed
+		query = "SELECT sum(t1.current_output*10800/1000000) as result from (select * from future_data where city = 'Heidelberg' order by curr_timestamp desc, weather_date asc limit 40) t1" #actual SQL statement to be executed
 		lines = cursor.execute(query) #execute the query
 		data = cursor.fetchall()
 		temp=data
