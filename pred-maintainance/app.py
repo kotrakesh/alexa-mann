8473464879df5039796c89431fe97aa7db50ba6a
megharangaswamy <meghagowda5193@gmail.com>
2017-10-16 15:47:46 +0200

    added comments

--- pred-maintainance/app.py
+++ pred-maintainance/app.py
@@ -11,75 +11,75 @@ dbco = dbc()
 
 
 @ask.launch
-def predective_maintenance():
+def predective_maintenance(): #Basic help
     initial_help = render_template('initialhelp')
     return question(initial_help)
 
 
 @ask.intent("YesterdayIntent")
-def past_yesterday():
+def past_yesterday():# To retrieve yesterday current
     currentRecieved = dbco.pastYesterday()
     pastCurrent = render_template('yesterdayCurrent', numbers=currentRecieved)
     return statement(pastCurrent)
 
 
 @ask.intent("LastweekIntent")
-def past_week():
+def past_week():#To retrieve last week current
     currentRecieved = dbco.pastlastWeek()
     pastWeekCurrent = render_template('lastweekCurrent', numbers=currentRecieved)
     return statement(pastWeekCurrent)
 
 
 @ask.intent("TillnowcurrentIntent")
-def present_tillNow():
+def present_tillNow():#To retrieve current till the current time
     currentRecieved = dbco.tillNowCurrent()
     presentTillNowCurrent = render_template('todayCurrentTillNow', numbers=currentRecieved)
     return statement(presentTillNowCurrent)
 
 
 @ask.intent("NowcurrentIntent")
-def present_now():
+def present_now():#To retrieve current at present time
     currentRecieved = dbco.currentNow()
     presentNowCurrent = render_template('todayCurrentNow', numbers=currentRecieved)
     return statement(presentNowCurrent)
 
 
 @ask.intent("TodayaftercurrentIntent")
-def present_after():
+def present_after():# To retrieve current expected for today
     currentRecieved = dbco.todayAfterCurrent()
     presentAfterCurrent = render_template('todayCurrentAfter', numbers=currentRecieved)
     return statement(presentAfterCurrent)
 
 
 @ask.intent("TodayweatherIntent")
-def present_weather():
+def present_weather():#To retrieve today's weather data
     weatherRecieved = dbco.todayWeather()
     descRecieved = dbco.todayWeatherDesc()
     presentWeather = render_template('todayWeather', numbers=weatherRecieved, desc=descRecieved)
     return statement(presentWeather)
 
 @ask.intent("NowweatherIntent")
-def present_Now_weather():
+def present_Now_weather():#To retrieve present weather data
     weatherRecieved = dbco.nowWeather()
     presentNowWeather = render_template('todayWeatherNow', numbers=weatherRecieved[0], desc=weatherRecieved[1], city=weatherRecieved[2])
     return statement(presentNowWeather)
 
 @ask.intent("TomorrowcurrentIntent")
-def future_tomorrow_current():
+def future_tomorrow_current():#To retrieve tomorrow expected current
     currentRecieved = dbco.tomorrowCurrent()
     futureTomorrowCurrent = render_template('tomorrowCurrent', numbers=currentRecieved)
     return statement(futureTomorrowCurrent)
 
 
 @ask.intent("NextweekcurrentIntent")
-def future_week_current():
+def future_week_current():#To retrieve current expected for net 5 day's
     currentRecieved = dbco.next5DaysCurrent()
     futureWeekCurrent = render_template('nextweekCurrent', numbers=currentRecieved)
     return statement(futureWeekCurrent)
 
 
 @ask.intent("TomorroweatherIntent")
-def future_Tomorrow_weather():
+def future_Tomorrow_weather():#To retrieve tomorrow weather data
     weatherRecieved = dbco.tomorrowWeather()
     descRecieved = dbco.tomorrowWeatherDesc()
     futureTomorrowWeather = render_template('tomorrowWeather', numbers=weatherRecieved, desc=descRecieved)
@@ -93,14 +93,14 @@ def failure_weather():
 
 
 @ask.intent("NextweekweatherIntent")
-def future_Week_weather():
+def future_Week_weather():#To retrieve next week weather data
     weatherRecieved = dbco.Nnext5DaysWeather()
     futureWeekWeather = render_template('nextweekWeather', numbers=weatherRecieved)
     return statement(futureWeekWeather)
 
 
 @app.route("/")
-def admin():
+def admin():# ,ethod to route with localhost
     pastYesterday = dbco.pastYesterday()
     currentNow = dbco.currentNow()
     tomorrowCurrent = dbco.tomorrowCurrent()
