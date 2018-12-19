153ca41710fedc58025fcbd84c97ca242f9b84fd
Micha Wiegel <micha.wiegel@gmail.com>
2018-12-19 23:31:11 +0100

    Scripts for starting Alexa

---
+++ scripts/ngrok.sh
@@ -0,0 +1,6 @@
+#!/bin/bash          
+cd /home/micha/Programs
+./ngrok http 5000 
+
+
+
---
+++ scripts/startAlexa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash          
+
+
+cd /home/micha/alexa-mann/roommonitor/dashboard
+
+python3.6 flask_app.py
